dd329ac4d075565eac7634c0872f901c290d01c1
Gleb Ustimenko <gleb.us@me.com>
2012-11-01 00:07:29 +0200

    Initial Commit

---
+++ GSAfterInstallingDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GSAfterInstallingDemo
+//
+//  Created by Glebus on 01.11.12.
+//  Copyright (c) 2012 Domus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GSAfterInstallingDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GSAfterInstallingDemo
+//
+//  Created by Glebus on 01.11.12.
+//  Copyright (c) 2012 Domus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
